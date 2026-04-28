7a55375684ae7ffc22adeca736d30f5a829254d4
300novus <300novus@gmail.com>
2026-04-29 02:47:25 +0500

    44
    44

--- Запуск.bat
+++ Запуск.bat
@@ -1,4 +1,6 @@
 @echo off
+echo Устанавливаем нейросеть (если еще не установлена)...
+py -m pip install --upgrade google-generativeai pillow
 echo Запускаем Chingiskhan Pro V11...
 start http://127.0.0.1:5000
 py app.py
